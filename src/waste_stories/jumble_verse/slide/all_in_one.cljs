f95e161a466a788144a6895b2f4514a347723957
ash-mcc <ashley_mcclenaghan@mac.com>
2022-03-30 14:03:30 +0100

    adding missing src files

---
+++ src/waste_stories/jumble_verse/slide/all_in_one.cljs
@@ -0,0 +1,63 @@
+(ns waste-stories.jumble-verse.slide.all-in-one)
+
+(def slide
+  [:section 
+   [:h4 "Direction from the jumble"]
+
+   [:div.r-stack
+    [:span.fragment.fade-out {:data-fragment-index 1}
+     [:img {:height 250 :src "img/waste-stories-banner.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 3}
+     [:img.fragment.fade-in {:data-fragment-index 1 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/bags-stone-seat-lower-back-walk2.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 5}
+     [:img.fragment.fade-in {:data-fragment-index 3 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/sq_soldiernurdles.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 7}
+     [:img.fragment.fade-in {:data-fragment-index 5 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/IMG_20210414_101027.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 9}
+     [:img.fragment.fade-in {:data-fragment-index 7 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/pickaxe-from-skip.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 11}
+     [:img.fragment.fade-in {:data-fragment-index 9 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/Springkerse-energy-drinks-100621.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 13}
+     [:img.fragment.fade-in {:data-fragment-index 11 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/BrianP_farm6_20210908_121003.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 15}
+     [:img.fragment.fade-in {:data-fragment-index 13 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/GorgeousGirlfriend-church-260222-e1646818003996.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 17}
+     [:img.fragment.fade-in {:data-fragment-index 15 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/sq_gun-e1647359424502.jpg"}]]
+    [:span.fragment.fade-out {:data-fragment-index 19}
+     [:img.fragment.fade-in {:data-fragment-index 17 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/Photo0279.jpg"}]]
+    [:img.fragment.fade-in {:data-fragment-index 19 :height 250 :src "https://cpb-eu-w2.wpmucdn.com/campuspress.stir.ac.uk/dist/e/546/files/2022/03/KingsKnot-sunglasses-300521.jpg"}]]
+
+   [:small
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 1} "A jumble of waste"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 2} " ...a jumble of thoughts."] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 3} "Plastic soldier pointing"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 4} " ...\"Choose some direction!\""] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 5} "An axe"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 6} " ...is material really axed?"] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 7} "Next, a pick"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 8} " ...ok(!), let's dig deeper."] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 9} "Twisted mech"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 10} " ...with more potential."] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 11} "A 45 year old Claas combine"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 12} " ...let's thresh this out."] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 13} "Teddy and a heart"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 14} " ...is this humanity?"] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 15} "Some gun shaped plastic"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 16} " ...appearances change, material less so."] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 17} "Scrapped cars"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 18} " ...journey's end!"] [:br]
+
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 19} "Re-flections in a lens"] 
+    [:span.fragment.fade-in-then-semi-out {:data-fragment-index 20} " ...need to re-look, re-think, re-use."] [:br] [:br]
+    
+    [:span.fragment.fade-in {:data-fragment-index 21 :style "color: lightgray"} "The End."]]
+   
+   ])  
---
+++ src/waste_stories/jumble_verse/slides.cljs
@@ -0,0 +1,7 @@
+(ns waste-stories.jumble-verse.slides
+  (:require [waste-stories.jumble-verse.slide.all-in-one :as all-in-one]))
+
+(defn all
+  "The slides which form the presentation of a verse that was written for the Waste Stories project"
+  []
+  [all-in-one/slide])
